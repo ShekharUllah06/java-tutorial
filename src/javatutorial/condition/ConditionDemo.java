1b68fce80486f9ed8e4d6974c029d92685469c16
Shekhar Ullah <ullah.shekhar@gmail.com>
2016-12-13 17:32:25 +0600

    Condition Demo class is added

---
+++ src/javatutorial/condition/ConditionDemo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatutorial.condition;
+
+/**
+ *
+ * @author Shekhar
+ */
+public class ConditionDemo {
+
+    public static void main(String[] args) {
+        int number1 = 50;
+        if (number1 < 33) {
+            System.out.println("Fail");
+        }
+        if (number1 >= 33) {
+            System.out.println("Pass");
+        }
+    }
+}
