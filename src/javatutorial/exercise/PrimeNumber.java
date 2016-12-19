aa82266adf30d40fe3649659fba9cb111949d150
Shekhar Ullah <ullah.shekhar@gmail.com>
2016-12-19 22:21:50 +0600

    Prime Number program added

---
+++ src/javatutorial/exercise/PrimeNumber.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatutorial.exercise;
+
+/**
+ *
+ * @author Shekhar
+ */
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            boolean prime = true;
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime) {
+                System.out.println(i);
+            }
+        }
+    }
+}
