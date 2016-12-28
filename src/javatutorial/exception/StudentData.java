b70cb73f8d6f9db8383435d3bb52386dddfb276a
Shekhar Ullah <ullah.shekhar@gmail.com>
2016-12-28 18:52:00 +0600

    Simple Calculator

---
+++ src/javatutorial/exception/StudentData.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatutorial.exception;
+
+/**
+ *
+ * @author Shekhar
+ */
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class StudentData{
+	int studentId;
+	String studentName;
+	StudentData(int studentId, String studentName){
+		this.studentId = studentId;
+		this.studentName = studentName;
+	}
+	public void display(){
+		System.out.println(studentId+" - "+studentName);
+	}
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		try{
+			int sId = scanner.nextInt();
+			String sName = scanner.next();
+			StudentData s1 = new StudentData(sId, sName);
+			s1.display();
+		}catch(InputMismatchException ime){
+			System.out.println("Enter valid Number like 0-9");
+		}
+
+
+	}
+}
