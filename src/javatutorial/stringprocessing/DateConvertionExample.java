9ca317acf83565ff7b034fd1959b30744c2aad45
Shekhar Ullah <ullah.shekhar@gmail.com>
2017-02-08 18:04:05 +0600

    Java io example

--- src/javatutorial/stringprocessing/DateConvertionExample.java
+++ src/javatutorial/stringprocessing/DateConvertionExample.java
@@ -14,15 +14,27 @@ import java.util.Date;
  * @author Shekhar
  */
 public class DateConvertionExample {
+
     public static void main(String[] args) {
-        SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
-        String dateInString = "7-Jun-2013";
+        SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy");
+        String dateInString = "7 Jun 2013";
 
         try {
 
             Date date = formatter.parse(dateInString);
             System.out.println(date);
             System.out.println(formatter.format(date));
+
+            SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd");
+
+            long d1 = formater.parse("2001-1-1").getTime();
+            long d2 = formater.parse("2004-8-2").getTime();
+            long totalDay=Math.abs((d1 - d2) / (1000 * 60 * 60 * 24));
+            int year=(int)totalDay/365;
+            int month=((int)totalDay%365)/30;
+            int day=((int)totalDay%365)% 30;
+            System.out.println(totalDay);
+            System.out.println("Year: "+year+" Month: "+ month + " day:"+ day);
 
         } catch (ParseException e) {
             e.printStackTrace();
--- src/javatutorial/stringprocessing/MyFileReaderEx.java
+++ src/javatutorial/stringprocessing/MyFileReaderEx.java
@@ -9,6 +9,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.StringTokenizer;
 
 public class MyFileReaderEx {
@@ -19,23 +22,37 @@ public class MyFileReaderEx {
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
             String myDocument;
-
+            SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy");
             while ((myDocument = br.readLine()) != null) {
                 StringTokenizer stringTokenizer = new StringTokenizer(myDocument, "/");
                 if (stringTokenizer.hasMoreElements()) {
                     System.out.println("Name : " + stringTokenizer.nextElement());
                 }
+                Date dateOfBirth = null;
+                Date dateOfDeath = null;
+                
                 if (stringTokenizer.hasMoreElements()) {
-                    System.out.println("Date of Birth : " + stringTokenizer.nextElement());
+                    dateOfBirth = sdf.parse(stringTokenizer.nextElement().toString());
+                    System.out.println("Date of Birth : " +sdf.format(dateOfBirth) );
                 }
                 if (stringTokenizer.hasMoreElements()) {
-                    System.out.println("Date of Death : " + stringTokenizer.nextElement());
+                    dateOfDeath = sdf.parse(stringTokenizer.nextElement().toString());
+                    System.out.println("Date of Death : " +sdf.format(dateOfDeath));
                 }
+                long date1 = dateOfBirth.getTime();
+                long date2 = dateOfDeath.getTime();
+                long totalDay =Math.abs((date1 - date2)/(1000 * 60 * 60 * 24));
+                int year = (int)totalDay/365;
+                int month=((int)totalDay%365)/30;
+                int finalDay = ((int)totalDay%365)%30;
+                System.out.println("Age : "+year+" Year "+month+" Month "+finalDay+" Days.");
                 System.out.println("...............................");
             }
         } catch (IOException ioe) {
             System.out.println("Exception Occured.");
             ioe.printStackTrace();
+        }catch(ParseException pe){
+        
         }
     }
 }
